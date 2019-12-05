63de4ade8be76b6d88550109c928de6a7d058d57
Mystery0 <mystery0dyl520@gmail.com>
2019-12-05 20:26:19 +0800

    fix: 异常处理添加服务异常的处理

--- src/main/java/vip/mystery0/base/springboot/model/ServiceApiException.java
+++ src/main/java/vip/mystery0/base/springboot/model/ServiceApiException.java
@@ -6,9 +6,13 @@ import vip.mystery0.tools.kotlin.model.Response;
  * @author mystery0
  */
 public class ServiceApiException extends RuntimeException {
-    private Response response;
+    private Response<?> response;
 
-    public ServiceApiException(Response response) {
+    public ServiceApiException(Response<?> response) {
         this.response = response;
+    }
+
+    public Response<?> getResponse() {
+        return response;
     }
 }
