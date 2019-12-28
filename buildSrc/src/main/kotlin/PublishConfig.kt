58fe6f07cfc6c49f65b245befea04c3c2bf7eedd
Mystery0 <mystery0dyl520@gmail.com>
2019-12-28 14:10:00 +0800

    release: 0.3.6

--- buildSrc/src/main/kotlin/PublishConfig.kt
+++ buildSrc/src/main/kotlin/PublishConfig.kt
@@ -5,7 +5,7 @@
 object PublishConfig {
     const val POM_GROUP_ID = "vip.mystery0"
     const val POM_ARTIFACT_ID = "base-spring-boot-starter"
-    const val POM_VERSION = "0.3.5"
+    const val POM_VERSION = "0.3.6"
 
     const val POM_REPO_NAME = "tools"
     const val POM_USER_ORG = "mystery00"
