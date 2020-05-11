1390939a91e6c8e421716f0a0ef80b6b9678a512
Mystery0 <mystery0dyl520@gmail.com>
2020-05-11 22:25:55 +0800

    fix: 更新依赖库版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,17 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-        classpath("org.jetbrains.kotlin:kotlin-allopen:1.3.50")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:1.3.72")
     }
 }
 
 plugins {
-    id("org.springframework.boot") version "2.2.5.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.springframework.boot") version "2.2.7.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("com.github.johnrengelman.shadow") version "5.2.0"
-    kotlin("jvm") version "1.3.61"
-    kotlin("plugin.spring") version "1.3.61"
+    kotlin("jvm") version "1.3.72"
+    kotlin("plugin.spring") version "1.3.72"
 }
 
 group = "vip.mystery0"
@@ -27,23 +27,21 @@ val nexusUrl = System.getenv("NEXUS_URL") ?: PublishConfig.NEXUS_URL
 
 repositories {
     maven("$nexusUrl/repository/maven-public/")
-    maven("$nexusUrl/repository/maven-releases/")
-    maven("$nexusUrl/repository/maven-snapshots/")
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
-    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
-    compileOnly("vip.mystery0.tools:java.tools:1.2.8")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
+    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
+    compileOnly("vip.mystery0.tools:java.tools:1.2.9")
     compileOnly("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.springframework.data:spring-data-redis")
     compileOnly("org.springframework:spring-aspects")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
-    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
-    testImplementation("vip.mystery0.tools:java.tools:1.2.8")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
+    testImplementation("vip.mystery0.tools:java.tools:1.2.9")
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("junit:junit:4.13")
