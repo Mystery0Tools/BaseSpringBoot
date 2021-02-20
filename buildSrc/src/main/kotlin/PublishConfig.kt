52ab8872cd8e2b3a1df13903a10aff861c760f39
dengyilin㋡ <dengyilin@xylink.com>
2021-02-20 17:58:45 +0800

    fix: 修复运行错误

--- buildSrc/src/main/kotlin/PublishConfig.kt
+++ buildSrc/src/main/kotlin/PublishConfig.kt
@@ -9,7 +9,7 @@ import org.gradle.kotlin.dsl.get
  */
 object PublishConfig {
     private const val POM_GROUP_ID = "vip.mystery0.tools"
-    private const val POM_VERSION = "1.0.0"
+    private const val POM_VERSION = "1.0.1"
 
     private const val PROJECT_NAME = "base-spring-boot-starter"
     private const val PROJECT_DESCRIPTION = "SpringBoot basic framework"
